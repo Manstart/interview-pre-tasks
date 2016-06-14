17fc916d8541f84e9a18efe2fcee52d47f8c2c61
Manstart <444768411@qq.com>
2016-06-14 22:00:09 +0800

    张旭熳+444768411@qq.com

--- my-task3/tab.js
+++ my-task3/tab.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2016-06-14 16:22:48
 * @Last Modified by:   Marte
-* @Last Modified time: 2016-06-14 21:39:58
+* @Last Modified time: 2016-06-14 21:59:42
 */
 var curindex = 0;
 $(function(){
@@ -11,7 +11,7 @@ $(function(){
 
 $('#tabs ul li').click(function() {
     curindex = $(this).index();
-    //console.log(curindex);
+    console.log(curindex);
     $(this).parent().parent().children('div').children('div').hide();
     $(this).parent().parent().children('div').children('div').eq(curindex).show();
     $(this).parent().children().removeClass('tab_active');
@@ -24,26 +24,15 @@ $('.cont1').mouseover(function(){
     $(this).parent().children().removeClass('tab_active');
     $(this).addClass('tab_active');
 })
-// $('.cont1').mouseout(function() {
-//     $(this).css({background: '#544f6b'});
-// });
 $('.cont2').mouseover(function(){
     $(this).parent().parent().children('div').children('div').hide();
     $(this).parent().parent().children('div').children('div').eq(1).show();
     $(this).parent().children().removeClass('tab_active');
     $(this).addClass('tab_active');
-    // $(this).css({background: '#8478b3'});
 })
-// $('.cont2').mouseout(function() {
-//     $(this).css({background: '#544f6b'});
-// });
 $('.cont3').mouseover(function(){
     $(this).parent().parent().children('div').children('div').hide();
     $(this).parent().parent().children('div').children('div').eq(2).show();
     $(this).parent().children().removeClass('tab_active');
     $(this).addClass('tab_active');
-    // $(this).css({background: '#8478b3'});
 })
-// $('.cont3').mouseout(function() {
-//     $(this).css({background: '#544f6b'});
-// });
